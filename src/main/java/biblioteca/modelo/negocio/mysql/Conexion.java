7eeab765e6158ebf91e00b2e89e5a7146bc0653e
Juan Rafael Ibáñez García <jribanezgarcia@gmail.com>
2026-03-21 16:20:14 +0100

    Implementada la clase Conexion

---
+++ src/main/java/biblioteca/modelo/negocio/mysql/Conexion.java
@@ -0,0 +1,45 @@
+package biblioteca.modelo.negocio.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLOutput;
+
+public class Conexion {
+
+    private static final String HOST ="localhost";
+    private static final String ESQUEMA="biblioteca";
+    private static final String USUARIO="admin";
+    private static final String CONTRASENA="biblioteca-2026";
+
+    private static Connection conexion = null;
+    //evitamos que se instancie
+    private Conexion() {
+
+    }
+    public static Connection establecerConexion(){
+        if(conexion!=null){
+            return conexion;
+        }
+        try{
+            conexion= DriverManager.getConnection("jdbc:mysql://"+HOST+"/"+ESQUEMA,USUARIO,CONTRASENA);
+            System.out.println("Conexion a Base de Datos realizada correctamente");
+        }catch (SQLException e){
+            System.out.println("ERROR MySQL:  " + e.toString());
+        }
+        return conexion;
+    }
+    public static void cerrarConexion(){
+        try{
+            if(conexion!=null){
+                conexion.close();
+                conexion = null;
+                System.out.println("Conexion a MySQL cerrada correctamente.");
+            }
+        }catch(SQLException e){
+            System.out.println("ERROR MySQL: "+ e.toString());
+        }
+    }
+
+
+}
